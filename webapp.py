bd37a6c8e144c6370c7d66be7790b50a69f24e75
anita <ruangroc@oregonstate.edu>
2020-06-13 15:25:13 -0700

    removed create and seed database files because I don't know if they're doing anything

--- webapp.py
+++ webapp.py
@@ -6,11 +6,5 @@ import sys
 import os
 
 if __name__ == '__main__':
-    if '--reset' in sys.argv:
-        with app.app_context():
-            createdatabase(app.config['DATABASE_URL'])
-            print('Database recreated')
-    else:
         host = os.environ.get('HOST', '0.0.0.0')
-        # port = int(os.environ.get('PORT', 5000))
         app.run(host=host)
